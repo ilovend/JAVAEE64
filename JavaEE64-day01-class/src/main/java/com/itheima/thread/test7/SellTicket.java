cf8479b3ea00e303ef6b092c6d8d94168ecdb02d
HeZhongYu <ilovend@qq.com>
2023-03-17 16:14:59 +0800

    2023-3-17 64期JAVASE课程:线程Thread,Callable,Runnable,同步锁

--- JavaEE64-day01-class/src/main/java/com/itheima/thread/test7/SellTicket.java
+++ JavaEE64-day01-class/src/main/java/com/itheima/thread/test7/SellTicket.java
@@ -9,29 +9,30 @@ package com.itheima.thread.test7;
  * @date 2023/03/17
  */
 public class SellTicket implements Runnable {
+    final Object obj = new Object();
     private int tickets = 100;//任务的总票数
 
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
 //            不管是哪个窗口买票,只要有票就可以卖
-            while (true) {
-//                卖完了就结束
-                if (tickets <= 0) {
-                    break;
+                while (true) {
+                    synchronized (obj) {
+                        //                卖完了就结束
+                        if (tickets <= 0) {
+                            break;
+                        }
+                        //                模拟卖票环节
+                        try {
+                            Thread.sleep(50);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        //                没卖完
+                        tickets--;
+                        System.out.println(Thread.currentThread().getName() + "正在买票,还剩" + tickets + "张票");
+                    }
                 }
-//                模拟卖票环节
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-//                没卖完
-                tickets--;
-                System.out.println(Thread.currentThread().getName() + "正在买票,还剩" + tickets + "张票");
-
-            }
-
         }
     }
 }
